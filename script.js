cdb888f48001b10ae3cbbe9686d700017700ba14
Pawan Bisht <112340386+IamPawan777@users.noreply.github.com>
2025-11-03 15:17:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function toCase(text) {
   // write your code here
+	let lower = text.lowerCase();
+	let upper = text.upperCase();
+	let arr = [lower, upper];
+	return arr.join("-");
 }
 
 // DO not change the code below
 
 const text = prompt("Enter text:");
 alert(toCase(text));
+
